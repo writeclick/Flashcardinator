150c64430b2778bbb3e81e462769fde6ebb17fad
Jeremy Lee James <jeremyleejames@gmail.com>
2012-10-22 16:49:44 -0700

    added attr_accessor to Card for :term and :definition

--- card.rb
+++ card.rb
@@ -1,5 +1,10 @@
 module Flashcardinator
   class Card
+    attr_accessor :term, :definition
 
+    def initialize(term = "sample_term", definition = "This is a sample defintiion.")
+      @term = term
+      @definition = definition
+    end
   end
 end
--- card_spec.rb
+++ card_spec.rb
@@ -6,11 +6,27 @@ module Flashcardinator
 
   describe Card do
 
+    let (:acard) { Card.new }
+
+    it 'responds to #term' do
+      acard.should respond_to :term
+    end
+
+    it 'responds to #definition' do
+      acard.should respond_to :definition
+    end
+
     describe '#initialize' do
       context 'after calling Card.new to generate a new card' do
-        it 'is a valid card'
+        it 'is a valid card' do
+          acard.should be_kind_of Card
+        end
+        it 'defaults @term to "sample_term"' do
+          acard.term.should eq("sample_term")
+        end
       end
     end
+
 
   end
 
